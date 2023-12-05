c6db95a3c621a83df4b94b8206333140ee9e4ee2
Lucas Rau <lucasmrau@gmail.com>
2023-12-05 16:43:54 -0500

    commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import  OneSignal from 'react-native-onesignal';
+
+import { Routes } from './src/routes';
+
+import { THEME } from './src/theme';
+import { Loading } from './src/components/Loading';
+
+import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('ac458a8a-d878-43be-b9f9-f6ba0083bda5')
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <CartContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </CartContextProvider>
+    </NativeBaseProvider>
+  );
+}
